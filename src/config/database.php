9f67956213ca7607527a70321700f00e53704f74
wallacecnzto <wallacewvinfo@gmail.com>
2020-03-22 21:34:06 -0300

    Initial commit: Create database connection

---
+++ src/config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database {
+
+    public static function getConnection() {
+
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath);
+        $conn = new mysqli($env['host'],
+                          $env['username'],
+                          $env['password'],
+                          $env['database']
+                        );
+
+        if($conn->connect_error) {
+            die("Error: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+
+}
